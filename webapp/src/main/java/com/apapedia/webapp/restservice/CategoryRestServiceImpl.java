0bfa53bc1084d057f8d912b0e644e1e24f43903d
Son Sulung Suryahatta Asnan <son.sulung@ui.ac.id>
2023-12-14 12:29:39 +0700

    tes

--- webapp/src/main/java/com/apapedia/webapp/restservice/CategoryRestServiceImpl.java
+++ webapp/src/main/java/com/apapedia/webapp/restservice/CategoryRestServiceImpl.java
@@ -15,7 +15,7 @@ public class CategoryRestServiceImpl implements CategoryRestService{
     public List<CategoryDTO> viewAllCategory(){
         Flux<CategoryDTO> categoryFlux = WebClient.create()
             .get()
-            .uri("http://localhost:8081/api/category/view-all")
+            .uri("http://sonsulung.com:10103/api/category/view-all")
             .retrieve()
             .bodyToFlux(CategoryDTO.class);
         return categoryFlux.collectList().block();
